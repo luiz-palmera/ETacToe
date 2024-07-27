913273ac08e934b659f9e77172393c130ca9c473
Luiz <luizrdamata@gmail.com>
2024-07-27 00:32:16 -0300

    enviando projeto inical

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Title } from './components/components'
+
+function App() {
+
+  return (
+    <>
+      <Title>E-TAC-TOE</Title>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/components.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Title = styled.h1`
+    font-size: 20px;
+    text-align: center;
+    color: #667761;  
+    font-weight: 800; 
+`
